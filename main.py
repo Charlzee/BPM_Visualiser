63424f84950b357017c59583fe78cb7da6b3af06
Charlzee <charlierw1028@gmail.com>
2026-02-08 13:53:36 +0000

    Enhance output formatting and structure; refactor song name and BPM retrieval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import signatures
 import time
+from colorama import Fore
 from pygame import mixer, quit
 
 mixer.init()
@@ -7,10 +8,19 @@ beat_sfx = mixer.Sound("beat.wav")
 end_beat_sfx = mixer.Sound("beat_end.wav")
 mixer.music.load("music.mp3")
 
-bpm = signatures.get("The Third Sanctuary", "bpm")[0]
-time_signatures = signatures.get("The Third Sanctuary", "signatures")[0]
+song_name = "The Third Sanctuary"
+bpm = signatures.get(song_name, "bpm")[0]
+time_signatures = signatures.get(song_name, "signatures")[0]
 
 mixer.music.play()
+
+def moveBack(amount):
+    return "\033[F" * amount
+
+def newLine(amount):
+    return "\n" * amount
+
+print(newLine(70))
 
 for numerator, denominator in time_signatures:
     if not mixer.music.get_busy():
@@ -18,7 +28,15 @@ for numerator, denominator in time_signatures:
 
     spb = 60 / bpm * (4 / denominator) # update seconds per beat
     for i in range(1, numerator + 1):
-        print(f"Beat {i}     ", end="\r")
+        print(f"""
+              {moveBack(7)}
+              |  {Fore.BLACK}Playing Song:{Fore.RESET}              
+              |  {Fore.YELLOW}{song_name}{Fore.RESET}
+              |  {Fore.GREEN}{bpm}BPM  {Fore.RED}{numerator}/{denominator}{Fore.RESET}              
+              |              
+              |     Beat {i}{Fore.BLACK}/{numerator}{Fore.RESET}              
+              """
+            , end="")
         
         if i == 1: # first beat of the measure
             end_beat_sfx.play()
